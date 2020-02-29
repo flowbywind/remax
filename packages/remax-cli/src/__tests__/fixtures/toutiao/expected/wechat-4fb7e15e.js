6aebe3ebc79477cb87f65fec103eda036f6668a7
Wei Zhu <yesmeck@gmail.com>
2020-02-29 22:39:09 +0800

    refactor: 打包所有输出文件
    preserveModule 会导致 rollup 不记录模块依赖关系，而[切分 css 文件](#592)和分析
    页面依赖的小程序组件都需要有模块的依赖信息。
    
    另外 remax 跟支付宝小程序 IDE 同时 watch 时经常出现，小程序 IDE
    编译失败的情况，猜测是 remax 生成文件太多导致，去掉 preserveModule
    后实测下来小程序 IDE 没再出现编译失败的情况

---
+++ packages/remax-cli/src/__tests__/fixtures/toutiao/expected/wechat-4fb7e15e.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('./index-0fb77022.js');
+
+var View = function View() {};
+var Text = function Text() {};
+var FunctionalPageNavigator = function FunctionalPageNavigator() {};
+
+exports.FunctionalPageNavigator = FunctionalPageNavigator;
+exports.Text = Text;
+exports.View = View;
